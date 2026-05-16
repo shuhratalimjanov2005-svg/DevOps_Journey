93bdd5a14c40f917f7c356eb0299aca53f46d71d
procyona <procyona@student.21-school.ru>
2026-05-16 18:24:33 +0500

    Complete

--- Python_DevOps_Projects/deploy.sh
+++ Python_DevOps_Projects/deploy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-echo "🚀 Запуск сборки Docker Compose..."
+
+# 🔥 УМНАЯ ПРОВЕРКА: Если команды docker-compose нет, скрипт САМ её установит!
+if ! command -v docker-compose &> /dev/null
+then
+    echo "⚠️ Старый docker-compose не найден! Исправляем..."
+    sudo curl -L "https://github.com/docker/compose/releases/download/v2.24.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    echo "✅ docker-compose успешно установлен в систему!"
+fi
+
+echo " Запуск сборки Docker Compose..."
 docker-compose down
-docker-compose up --build -d
-echo "✅ Всё готово! Контейнеры подняты в фоне!"
+docker-compose up -d --build
+
+echo " Всё готово! Контейнеры подняты в фоне в твоей виртуалке!"
